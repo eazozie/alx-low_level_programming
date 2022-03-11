92e8f64b3f8f85da1b189f85d546ed20e5dff1fc
eazozie <azozie.ebube@gmail.com>
2022-03-11 07:26:24 +0100

    My low level c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Printf: Prints "with proper grammar, but the outcome is a piece of art
+ * Newline added
+ *  *
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
